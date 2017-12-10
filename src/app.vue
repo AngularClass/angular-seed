853592c28b56fc6ec84bc0e27c1045832cd33c66
Travis Ueki <travis@Traviss-MacBook.local>
2017-12-09 18:48:55 -0800

    :sparkles: root and wrapper components

--- src/app.vue
+++ src/app.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <ComponentWrapper />
   </div>
 </template>
 
 <script>
-import Hello from './components/hello'
-
+import ComponentWrapper from './components/ComponentWrapper'
 export default {
   name: 'app',
   components: {
-    Hello
+    ComponentWrapper
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="stylus" scoped></style>
